caf0dd43f13b3b07379697c2e50389d3946ef52e
Asia <asialuk98@gmail.com>
2018-05-10 10:04:27 +0200

    Add data print from file

--- src/data/Parser.java
+++ src/data/Parser.java
@@ -1,5 +1,92 @@
+
 package data;
 
-public class Parser {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.HashMap;
 
+
+public class Parser {
+	
+	
+	private File file;
+	private ArrayList<HashMap> allCities;
+	private HashMap<String, String> connections;
+	private HashMap<String, Integer> distances;
+	
+	public Parser() {
+		file = new File("zad4_data.txt");
+		allCities = new ArrayList<HashMap>();
+		connections = new HashMap<String, String>(12);
+		distances = new HashMap<String, Integer>(12);
+	}
+	
+	public Parser(String fileName) {
+		file = new File(fileName);
+		allCities = new ArrayList<HashMap>();
+		connections = new HashMap<String, String>(12);
+		distances = new HashMap<String, Integer>(12);
+	}
+
+
+	public void  read() {
+		
+		try{
+			
+            Scanner sc = new Scanner(file);
+            
+            String line = sc.nextLine();
+            String cityName;
+            int distance;
+            String [] info = new String[100];
+            int [] nums;
+           
+
+            while(sc.hasNext()) {
+
+	                if (line.matches(".*H.*")) {
+	                	
+	                    System.out.println(line);
+	                    line = sc.nextLine();
+	
+	                }
+
+	                
+	                while ((!line.matches(".*HZ.*") || !line.matches(".*HW.*") ||
+	                		   !line.matches(".*HC.*")) && sc.hasNext()) {
+	                		
+	                		if (!line.contains("-")) {
+		                    
+	                			System.out.println("blank line");
+	                			line = sc.nextLine();
+	                			break;
+	                		}
+	                	
+	                	                		
+		                		info = line.split("-|\\(|\\)");
+		                    System.out.print(info[0] + ".");
+		                    
+	                			info = line.split("-|\\(|\\)");
+		                    System.out.print(info[1] + ".");
+		                    
+		                    info = line.split("-|\\(|\\)");
+		                    System.out.println(info[2] + ".loop ended");
+
+		                    line = sc.nextLine();
+
+	                }
+                }
+            
+            sc.close(); 
+            
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+		
+	}
+    
 }
+
---
+++ src/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import data.Parser;
+import java.util.regex.*;
+import java.util.ArrayList;
+
+public class Main {
+	
+	public static void main (String [] args) {
+		
+		Parser parser = new Parser("zad4_data.txt");
+		parser.read();
+		
+		//String a = "Bree - Angmar (360)";
+	//	String [] b = new String [10];
+		// tokens.add(a.split("-|\\(|\\)"));
+		//String[]tokens = pdfName.split("-|\\.");
+		
+
+		//System.out.println(tokens.get(0)[0] + " " + tokens.get(0)[1] + " " + tokens.get(0)[2]);
+		
+		
+		
+	}
+}
